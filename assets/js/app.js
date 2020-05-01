e227ef83eb9d066304414c9879456cbc8089b51e
nplhse <mail@nplhse.com>
2020-05-01 12:08:05 +0200

    Added bootstrap to base theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,4 @@ import '../css/app.scss';
 import 'bootstrap';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+import $ from 'jquery';
