49a6fec8bfca26d0435ed43fcc8264ed37010a12
Christoph Steltner <mail@nplhse.com>
2020-05-02 21:09:40 +0200

    Added doctrine bundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
