ef2889040121a9e31e0c9c81b98be641a25c2388
Christoph Steltner <mail@nplhse.com>
2020-05-13 19:09:04 +0200

    Moved password hashing to Listener
    Also removed AdminController

--- src/Migrations/Version20200511160415.php
+++ src/Migrations/Version20200511160415.php
@@ -12,24 +12,24 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200511160415 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
         $this->addSql('CREATE TABLE `rememberme_token` (`series` char(88) UNIQUE PRIMARY KEY NOT NULL, `value` char(88) NOT NULL, `lastUsed` datetime NOT NULL, `class` varchar(100) NOT NULL, `username` varchar(200) NOT NULL)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
         $this->addSql('DROP TABLE `rememberme_token`');
