0d5d65d376b7da80f61ea7c3cfe5ca7b3d92d9dc
Christoph Steltner <mail@nplhse.com>
2020-05-14 22:35:04 +0200

    Added copyToClipboard for compiled letters

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,9 +11,31 @@ import '../css/main.scss';
 // Add jQuery
 import $ from 'jquery';
 
+// Add bootstrap+fontawsome
 require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+// Add ClipboardJS
+import ClipboardJS from 'clipboard';
 
 // Activate jQuery popover
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+});
+
+var clipboard = new ClipboardJS('.btn');
+
+// Add some debugging for the clipboard
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
 });
