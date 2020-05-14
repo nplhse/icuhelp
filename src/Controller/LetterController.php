df54dbf7db1510fef85900af95e5d52d44d03536
Christoph Steltner <mail@nplhse.com>
2020-05-14 21:22:52 +0200

    Added snippets to frontend

--- src/Controller/LetterController.php
+++ src/Controller/LetterController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Snippet;
 use App\Form\LetterType;
+use App\Form\SnippetFormType;
+use App\Repository\SnippetRepository;
 use App\Service\LetterBuilder;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,6 +33,67 @@ class LetterController extends AbstractController
         }
 
         return $this->render('letter/form.html.twig', [
+            'form' => $form->createView(),
+            'errors' => $form->getErrors(true, false),
+        ]);
+    }
+
+    /**
+     * @Route({"de": "/arztbriefe/textbausteine", "en": "/letter/snippets"}, name="snippet_list")
+     * @IsGranted("ROLE_USER")
+     */
+    public function listSnippets(Request $request, SnippetRepository $snippetRepository)
+    {
+        return $this->render('letter/snippets/list.html.twig', [
+            'snippets' => $snippetRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route({"de": "/arztbriefe/textbausteine/neu", "en": "/letter/snippets/new"}, name="snippet_new")
+     * @IsGranted("ROLE_USER")
+     */
+    public function newSnippet(Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(SnippetFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $snippet = $form->getData();
+
+            $em->persist($snippet);
+            $em->flush();
+
+            $this->addFlash('success', 'msg.snippet.added');
+
+            return $this->redirectToRoute('snippet_list');
+        }
+
+        return $this->render('letter/snippets/new.html.twig', [
+            'form' => $form->createView(),
+            'errors' => $form->getErrors(true, false),
+        ]);
+    }
+
+    /**
+     * @Route({"de": "/arztbriefe/textbausteine/{id}", "en": "/letter/snippets/{id}"}, name="snippet_edit")
+     * @IsGranted("ROLE_USER")
+     */
+    public function editSnippet(Snippet $snippet, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(SnippetFormType::class, $snippet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($snippet);
+            $em->flush();
+
+            $this->addFlash('primary', 'msg.snippet.edited');
+
+            return $this->redirectToRoute('snippet_list');
+        }
+
+        return $this->render('letter/snippets/edit.html.twig', [
             'form' => $form->createView(),
             'errors' => $form->getErrors(true, false),
         ]);
