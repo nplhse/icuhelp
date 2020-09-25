a6c49a091ba2ad30f7789e8c9c3c49273ba472f6
nplhse <mail@nplhse.com>
2020-09-25 12:17:41 +0200

    Trying to make migrations work again

--- src/Migrations/Version20200924101347.php
+++ src/Migrations/Version20200924101347.php
@@ -20,7 +20,7 @@ final class Version20200924101347 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE migration_versions');
+        $this->addSql('DROP TABLE migration_versions IF EXISTS');
     }
 
     public function down(Schema $schema) : void
