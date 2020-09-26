07695832d3b2847bd204ed56ad8643f2cb11af19
nplhse <mail@nplhse.com>
2020-09-26 13:20:31 +0200

    Fixed some code styling issues

--- src/Controller/Admin/SOPCrudController.php
+++ src/Controller/Admin/SOPCrudController.php
@@ -3,12 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\SOP;
-use App\Entity\SOPTag;
-use App\Form\SOPTagType;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
