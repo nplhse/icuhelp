c719a3f032be0c4aee83fcc56fc33a8c07dc9d0e
Christoph Steltner <mail@nplhse.com>
2020-09-27 23:26:46 +0200

    Had to remove orphan removal of Contacts as it was causing some major issues...

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -46,7 +46,7 @@ class Contact
     private $address;
 
     /**
-     * @ORM\ManyToOne(targetEntity=ContactCategory::class, inversedBy="category", orphanRemoval=true)
+     * @ORM\ManyToOne(targetEntity=ContactCategory::class, inversedBy="category")
      */
     private $category;
 
