1cfb9baa32c4d7d43575c63c82d29a3502230321
Christoph Steltner <mail@nplhse.com>
2020-12-27 12:57:35 +0100

    Fixed some styling issues

--- migrations/Version20201227113332.php
+++ migrations/Version20201227113332.php
@@ -12,18 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201227113332 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE snippet ADD priority INT DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE snippet DROP priority');
